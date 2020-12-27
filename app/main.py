c43de5ac2f424ee3af80dff5da4abc387db2c155
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-12-26 23:06:30 -0800

    added app folder

---
+++ app/main.py
@@ -0,0 +1,65 @@
+import pickle
+import numpy as np
+
+import flask
+from flask import render_template, request
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity as cos_sim
+from flask import Flask
+
+with open("pickles/full_cv.pkl", "rb") as f:
+    df = pickle.load(f)
+
+with open("pickles/vectorizer.pkl", "rb") as f:
+    tfidf_v = pickle.load(f)
+
+with open("pickles/X_tfidf.pkl", "rb") as f:
+    X_tfidf = pickle.load(f)
+
+
+def inference(abstract, top_k=5):
+
+    x = tfidf_v.transform([abstract])
+    sims = cos_sim(X_tfidf, x)
+    sims = np.squeeze(sims)
+    most_sim_idx = sims.argsort()[::-1]  # reverse
+    # if we also want to reject the paper itself, then it'd be most_sim_idx[-2::-1]
+
+    df_fajne = df.iloc[most_sim_idx[:top_k]]
+
+    return df_fajne
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main_handler():
+    return render_template("main.html")
+
+
+@app.route("/recommend", methods=["POST"])
+def rec_handler():
+
+    abstract = request.get_json()["abstract"]
+    # abstract = request.form['abstract']
+
+    df = inference(abstract)
+
+    if len(df) < 1:
+        return "No papers found.", 404
+    else:
+        r = {
+            "ids": df["id"].tolist(),
+            "titles": df["title"].tolist(),
+            "authors": df["authors"].tolist(),
+            "abstracts": df["abstract"].tolist(),
+        }
+
+        return flask.json.jsonify(r), 200
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
